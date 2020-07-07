a9f0ad2a95fb2c61bacf3c34ca624cfa31022e64
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-07 01:08:46 +0000

    Deploy PeggyLan/pomodoro to github.com/PeggyLan/pomodoro.git:gh-pages

---
+++ precache-manifest.1e9188b80d6e7a572a4f6e2bd2abc25b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "alarms/alarm1.mp3"
+  },
+  {
+    "revision": "20024729502f570ac2170f626df28a29",
+    "url": "alarms/child.mp3"
+  },
+  {
+    "revision": "848b70144db8e24fbcdac2490e6a459d",
+    "url": "alarms/haha.mp3"
+  },
+  {
+    "revision": "74f5a2043b5b63c4efffb1af3b7a2cdc",
+    "url": "alarms/lionking.mp3"
+  },
+  {
+    "revision": "8c0666bf84dddd9511cc408b1b04598d",
+    "url": "alarms/whatsapp.mp3"
+  },
+  {
+    "revision": "7bb5f53793d899db68f6",
+    "url": "css/app.5ec7a04f.css"
+  },
+  {
+    "revision": "d7a6ad58c3e64ef1324b",
+    "url": "css/chunk-vendors.b32f2eeb.css"
+  },
+  {
+    "revision": "7f53c79aa9f6bfe6b6365390ee7de526",
+    "url": "fonts/architects-daughter-latin-400-normal.7f53c79a.woff2"
+  },
+  {
+    "revision": "83db1f979f2676c9c397646bcb6fe5f7",
+    "url": "fonts/architects-daughter-latin-400-normal.83db1f97.woff"
+  },
+  {
+    "revision": "3567310d00535392101f734c3173e268",
+    "url": "index.html"
+  },
+  {
+    "revision": "7bb5f53793d899db68f6",
+    "url": "js/app.33343043.js"
+  },
+  {
+    "revision": "d7a6ad58c3e64ef1324b",
+    "url": "js/chunk-vendors.4e7fedc1.js"
+  },
+  {
+    "revision": "13c322d211b69b10342b",
+    "url": "js/list.88c35e8d.js"
+  },
+  {
+    "revision": "2d465db806384fbb1c3a",
+    "url": "js/notfound.c837472f.js"
+  },
+  {
+    "revision": "35058384cfbe544c7153",
+    "url": "js/settings.0d77c70e.js"
+  },
+  {
+    "revision": "3dc210960fe7e4127773465ac5d8a550",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
